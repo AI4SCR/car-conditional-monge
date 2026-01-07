b13b557ca50cc90870f07f08e335d301d2d7187f
Alice Driessen <alicedriessen@Alices-MacBook-Air.local>
2026-01-07 21:10:54 +0100

    Formatting

--- scripts/train_cond_car.py
+++ scripts/train_cond_car.py
@@ -28,7 +28,7 @@ def train_ood_monge(config_path: Path):
 
     config = load_config(config_path)
     # job_id = get_environ_var("LSB_JOBID", fail_gracefully=True)
-    job_id=1
+    job_id = 1
 
     if len(config.logger_path) != 0:
         logger_path = Path(config.logger_path)
